19c2bca60115c0039b770d34bd1a1ce5d5a81a0f
JS-404-000 <4014eogus@gmail.com>
2023-04-12 11:14:43 +0900

    commit2 -- second commit

--- app/src/main/java/com/example/information/MainActivity.java
+++ app/src/main/java/com/example/information/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.information;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Instrumentation;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
     }
 }
